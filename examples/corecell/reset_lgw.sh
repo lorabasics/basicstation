6c604c37aaf878f462dfd2bb60ed6b417ce67dd3
mpous <marc@balena.io>
2020-09-18 20:02:58 +0200

    More changes corecell

--- examples/corecell/reset_lgw.sh
+++ examples/corecell/reset_lgw.sh
@@ -21,16 +21,17 @@ WAIT_GPIO() {
 init() {
     # setup GPIOs
     echo "$SX1302_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/export; WAIT_GPIO
+    #echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/export; WAIT_GPIO
 
     # set GPIOs as output
     echo "out" > /sys/class/gpio/gpio$SX1302_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction; WAIT_GPIO
+    #echo "out" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction; WAIT_GPIO
 }
 
 reset() {
     echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
     echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
+    echo "SX1302 reset through GPIO$SX1302_RESET_PIN..."
 
     # write output for SX1302 CoreCell power_enable and reset
     echo "1" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value; WAIT_GPIO
@@ -45,10 +46,10 @@ term() {
     then
         echo "$SX1302_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
     fi
-    if [ -d /sys/class/gpio/gpio$SX1302_POWER_EN_PIN ]
-    then
-        echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
+    #if [ -d /sys/class/gpio/gpio$SX1302_POWER_EN_PIN ]
+    #then
+    #    echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+    #fi
 }
 
 case "$1" in
--- start.sh
+++ start.sh
@@ -22,19 +22,19 @@ declare -a pinToGPIO
 pinToGPIO=( -1 -1 -1 2 -1 3 -1 4 14 -1 15 17 18 27 -1 22 23 -1 24 10 -1 9 25 11 8 -1 7 0 1 5 -1 6 12 13 -1 19 16 26 20 -1 21)
 
 
-cd examples/live-s2.sm.tc
+cd examples/corecell
 
 # Default to TTN server
 TC_URI=${TC_URI:-"wss://lns.eu.thethings.network:443"} 
-TC_TRUST=${TC_TRUST:-$(curl https://letsencrypt.org/certs/trustid-x3-root.pem.txt)}
+TC_TRUST=${TC_TRUST:-$(curl https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt)}
 
 
-GW_RESET_PIN=${GW_RESET_PIN:-11}
+GW_RESET_PIN=${GW_RESET_PIN:-22}
 GW_RESET_GPIO=${GW_RESET_GPIO:-${pinToGPIO[$GW_RESET_PIN]}}
 
 # Setup TC files from environment
-echo $TC_URI > tc.uri
-echo "$TC_TRUST" > tc.trust
+echo $TC_URI > ./lns-ttn/tc.uri
+echo "$TC_TRUST" > ./lns-ttn/tc.trust
 
 # Reset gateway
 echo "Resetting gateway concentrator on GPIO $GW_RESET_GPIO"
@@ -45,5 +45,4 @@ echo 1 > /sys/class/gpio/gpio$GW_RESET_GPIO/value
 echo 0 > /sys/class/gpio/gpio$GW_RESET_GPIO/value
 echo $GW_RESET_GPIO > /sys/class/gpio/unexport
 
-RADIODEV=/dev/spidev0.0 ../../build-rpi-std/bin/station 
-
+./start-station.sh -l ./lns-ttn
