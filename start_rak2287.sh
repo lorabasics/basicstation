20e280507b0e110cf755a5ad0038641b2b02fc48
Xose Pérez <xose.perez@gmail.com>
2021-02-23 21:20:25 +0100

    Fixed regression in TTNv2 certificate request. New TTN_STACK_VERSION variable to auto-configure TTNv2 or TTS

--- start_rak2287.sh
+++ start_rak2287.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash 
 
-cd examples/corecell
+# Defaults to TTN server v2
+TTN_STACK_VERSION=${TTN_STACK_VERSION:-2}
+if [ $TTN_STACK_VERSION -eq 2 ]; then
+	TC_URI=${TC_URI:-"wss://lns.eu.thethings.network:443"} 
+	TC_TRUST=${TC_TRUST:-$(curl --silent "https://letsencrypt.org/certs/trustid-x3-root.pem.txt"))}
+elif [ $TTN_STACK_VERSION -eq 3 ]; then
+	TC_URI=${TC_URI:-"wss://eu1.cloud.thethings.network:8887"} 
+	TC_TRUST=${TC_TRUST:-$(curl --silent "https://letsencrypt.org/certs/{trustid-x3-root.pem.txt,isrgrootx1.pem}"))}
+else
+    echo -e "\033[91mERROR: Wrong TTN_STACK_VERSION value, should be either 2 o 3.\033[0m"
+	balena-idle
+fi
 
-# Default to TTN server
-TC_URI=${TC_URI:-"wss://lns.eu.thethings.network:443"} 
-TC_TRUST=${TC_TRUST:-$(curl --silent "https://letsencrypt.org/certs/{trustid-x3-root.pem.txt,isrgrootx1.pem}"))}
+# Check configuration
+if [ $TC_URI = "" ] || [ $TC_TRUST == "" ]; then
+    echo -e "\033[91mERROR: Missing configuration, define either TTN_STACK_VERSION or TC_URI and TC_TRUST.\033[0m"
+	balena-idle
+fi
 
 # declare map of hardware pins to GPIO on Raspberry Pi
 declare -a pinToGPIO
@@ -12,11 +25,11 @@ pinToGPIO=( -1 -1 -1 2 -1 3 -1 4 14 -1 15 17 18 27 -1 22 23 -1 24 10 -1 9 25 11 
 GW_RESET_PIN=${GW_RESET_PIN:-11}
 GW_RESET_GPIO=${GW_RESET_GPIO:-${pinToGPIO[$GW_RESET_PIN]}}
 
+cd examples/corecell
 
 # Setup TC files from environment
 echo "$TC_URI" > ./lns-ttn/tc.uri
 echo "$TC_TRUST" > ./lns-ttn/tc.trust
-
 if [ ! -z ${TC_KEY} ]; then
 	echo "Authorization: Bearer $TC_KEY" | perl -p -e 's/\r\n|\n|\r/\r\n/g'  > ./lns-ttn/tc.key
 fi
