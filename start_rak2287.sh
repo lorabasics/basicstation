6aa8f44d1cdd66d995c36c62260e6997ddc4430c
Xose Pérez <xose.perez@gmail.com>
2021-02-23 21:19:10 +0100

    GW_RESET_PIN and GW_RESET_GPIO variables working for RAK2287 concentrator

--- start_rak2287.sh
+++ start_rak2287.sh
@@ -6,6 +6,12 @@ cd examples/corecell
 TC_URI=${TC_URI:-"wss://lns.eu.thethings.network:443"} 
 TC_TRUST=${TC_TRUST:-$(curl --silent "https://letsencrypt.org/certs/{trustid-x3-root.pem.txt,isrgrootx1.pem}"))}
 
+# declare map of hardware pins to GPIO on Raspberry Pi
+declare -a pinToGPIO
+pinToGPIO=( -1 -1 -1 2 -1 3 -1 4 14 -1 15 17 18 27 -1 22 23 -1 24 10 -1 9 25 11 8 -1 7 0 1 5 -1 6 12 13 -1 19 16 26 20 -1 21)
+GW_RESET_PIN=${GW_RESET_PIN:-11}
+GW_RESET_GPIO=${GW_RESET_GPIO:-${pinToGPIO[$GW_RESET_PIN]}}
+
 
 # Setup TC files from environment
 echo "$TC_URI" > ./lns-ttn/tc.uri
@@ -15,6 +21,8 @@ if [ ! -z ${TC_KEY} ]; then
 	echo "Authorization: Bearer $TC_KEY" | perl -p -e 's/\r\n|\n|\r/\r\n/g'  > ./lns-ttn/tc.key
 fi
 
+# Set other environment variables
+export GW_RESET_GPIO=$GW_RESET_GPIO
 
 ./start-station.sh -l ./lns-ttn
 
